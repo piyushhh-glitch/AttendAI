daad26052b8fcc653620cf61f7ce14381b2c6bb9
piyushhh-glitch <piyushthakur1611@gmail.com>
2026-09-16 18:36:59 +0530

    Teacher and Student tabs

---
+++ src/components/footer.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown("""
+
+                <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+                    <p style="font-weight:bold; color:white">Created With ❤️ By Piyush</p>
+                </div>
+
+                """,unsafe_allow_html=True)
