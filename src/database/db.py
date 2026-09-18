143cddc088fb6e8699d735cc9142d5d88083034b
piyushhh-glitch <piyushthakur1611@gmail.com>
2026-09-18 17:15:14 +0530

    feat: add teacher subject management

--- src/database/db.py
+++ src/database/db.py
@@ -1,12 +1,14 @@
 from src.database.config import supabase
 import bcrypt
 
+
 def hash_pass(pwd):
     return bcrypt.hashpw(pwd.encode(),bcrypt.gensalt()).decode()
 
 
 def check_pass(pwd,hashed):
     return bcrypt.checkpw(pwd.encode(),hashed.encode())
+
 
 def check_teacher_exists(username):
     response=supabase.table("teachers").select("username").eq("username",username).execute()
@@ -36,3 +38,24 @@ def get_all_students():
 def create_student(new_name,face_embedding,voice_embedding=None):
     data={'name':new_name,'face_embedding':face_embedding,'voice_embedding':voice_embedding}
     response=supabase.table("students").insert(data).execute()
+
+
+def create_subject(subject_code,name,section,teacher_id):
+    data={'subject_code':subject_code,'name':name,'section':section,'teacher_id':teacher_id}
+    response=supabase.table("subjects").insert(data).execute()
+    return response.data
+
+def get_teacher_subjects(teacher_id):
+    response=supabase.table("subjects").select("*,subject_students(count),attendance_records(timestamp)").eq("teacher_id",teacher_id).execute()
+    subjects=response.data
+
+    for sub in subjects:
+        sub['total_students']=sub.get("subject_students",[{}])[0].get('count',0) if sub.get('subject_students') else 0
+        attendance=sub.get('attendance_records',[])
+        unique_sessions=len(set(log['timestamp'] for log in attendance))
+        sub['total_classes']=unique_sessions
+
+        sub.pop('subject_students',None)
+        sub.pop('attendance_records',None)
+
+    return subjects
--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -4,7 +4,8 @@ from src.ui.base_layout import style_background_dashboard,style_base_layout
 from src.components.footer import footer_dashboard
 from src.database.db import check_teacher_exists
 from src.database.db import create_teacher
-from src.database.db import teacher_login
+from src.database.db import teacher_login,get_teacher_subjects
+from src.components.dialog_create_subject import create_subject_dialog
 
 def teacher_screen():
     style_background_dashboard()
@@ -61,9 +62,9 @@ def teacher_dashboard():
 
     if st.session_state.current_teacher_tab=='take_attendance':
         teacher_tab_take_attendance()
-    if st.session_state.current_teacher_tab=='manage_subjects':
+    elif st.session_state.current_teacher_tab=='manage_subjects':
         teacher_tab_manage_subjects()
-    if st.session_state.current_teacher_tab=='attendance_records':
+    elif st.session_state.current_teacher_tab=='attendance_records':
         teacher_tab_attendance_records()
 
     footer_dashboard()
@@ -72,7 +73,37 @@ def teacher_tab_take_attendance():
     st.header('Take AI attendance')
 
 def teacher_tab_manage_subjects():
-    st.header('Manage Subjects')
+    teacher_id=st.session_state.teacher_data['teacher_id']
+    col1,col2=st.columns(2)
+
+    with col1:
+        st.header('Manage Subjects',width='stretch')
+    with col2:
+        if st.button('Create New Subjects',width='stretch'):
+            create_subject_dialog(teacher_id)
+
+    # List All Subjects
+    subjects=get_teacher_subjects(teacher_id)
+    if subjects:
+        for sub in subjects:
+            stats=[
+                ("👥","students",sub['total_students']),
+                ("🕐","classes",sub['total_classes'])
+            ]
+            def share_btn():
+                if st.button(f"Share Code : {sub['name']}",key=f"share_{sub['subject_code']}",icon=":material/share:"):
+                    share_subject_dialog(sub['name'],sub['subject_code'])
+                st.space()
+
+            subject_card(
+                name=sub['name'],
+                code=sub['subject_code'],
+                section=sub['section'],
+                stats=stats,
+                footer_callback=share_btn
+            )
+    else:
+        st.info("NO SUBJECTS FOUND. CREATE ONE ABOVE")
 
 def teacher_tab_attendance_records():
     st.header('Attendance Records')
