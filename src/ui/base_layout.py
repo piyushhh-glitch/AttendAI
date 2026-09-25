1a41fe65be4c7460ee254b47c9f720753d5d9463
piyushhh-glitch <piyushthakur1611@gmail.com>
2026-09-25 15:14:50 +0530

    ui update

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -66,6 +66,7 @@ def style_base_layout():
                 font-size: 3.5rem !important;
                 line-height:1.1 !important;
                 margin-bottom:0rem !important;
+                color: #1F1F1F !important;
             }
                 
 
@@ -74,11 +75,12 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                # color: #1F1F1F !important;
+                color: #1F1F1F !important;
             }
                 
             h3, h4, p {
                 font-family: 'Outfit', sans-serif !important;  
+                color: #1F1F1F !important;
             }
                 
 
